d2ca2f6d41fd2bc2edb449dea19ec297e8e6de57
VladokAR <vladokthrr@gmail.com>
2020-05-23 18:27:37 +0300

    Updates

---
+++ precache-manifest.85a781aedbb91bb3680092b271549efe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cdcd364f4a250b1d478ec468c52928",
+    "url": "/ReactCalculator/index.html"
+  },
+  {
+    "revision": "12e1d722575593b2c92a",
+    "url": "/ReactCalculator/static/css/main.261d6190.chunk.css"
+  },
+  {
+    "revision": "81ee469a064897b18869",
+    "url": "/ReactCalculator/static/js/2.d5f48272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCalculator/static/js/2.d5f48272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e1d722575593b2c92a",
+    "url": "/ReactCalculator/static/js/main.6b63f237.chunk.js"
+  },
+  {
+    "revision": "cb9359e4ecd7d08e7f52",
+    "url": "/ReactCalculator/static/js/runtime-main.443c3040.js"
+  },
+  {
+    "revision": "1f9927e6f5a6f827e665e3cf2f3604c2",
+    "url": "/ReactCalculator/static/media/Cloud_right_bottom.1f9927e6.png"
+  },
+  {
+    "revision": "81ab502fa47c79f508ac56f41b504624",
+    "url": "/ReactCalculator/static/media/astronaut.81ab502f.png"
+  },
+  {
+    "revision": "7a8340f59f996eb3edd8f8df00bd6331",
+    "url": "/ReactCalculator/static/media/bg.7a8340f5.png"
+  },
+  {
+    "revision": "7fb9e240448babc83993e2cc634db7a9",
+    "url": "/ReactCalculator/static/media/cloud_Left_Top.7fb9e240.png"
+  },
+  {
+    "revision": "636733558581d84ac684d001f318df7d",
+    "url": "/ReactCalculator/static/media/moon.63673355.png"
+  },
+  {
+    "revision": "c54641f6683263481fbaa8fe81d0b7d9",
+    "url": "/ReactCalculator/static/media/racket.c54641f6.png"
+  },
+  {
+    "revision": "19de45a392ba014cc9f1596115291c86",
+    "url": "/ReactCalculator/static/media/robotic.19de45a3.TTF"
+  }
+]);
